46c771692adb8d140b9cb77bead52e43f1716290
va3mezk <va3mezk@gmail.com>
2021-11-11 21:20:45 -0600

    update test II

--- src/test/kotlin/MultiplyTest.kt
+++ src/test/kotlin/MultiplyTest.kt
@@ -3,9 +3,11 @@ import kotlin.test.assertEquals
 
 class MultiplyTest {
     @Test
-    fun `test multiply *3`() = assertEquals(166833, Multiply().Operation(3))
+    fun `test multiply three`() = assertEquals(166833, Multiply().Operation(3))
 
     @Test
-    fun `test multiply *5`() = assertEquals(99500, Multiply().Operation(5))
+    fun `test multiply five`() = assertEquals(99500, Multiply().Operation(5))
+
+
 
 }
